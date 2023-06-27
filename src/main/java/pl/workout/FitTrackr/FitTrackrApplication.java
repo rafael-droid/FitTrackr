a40645562ffc2e54ec216dbc223e2f95eba572e4
s21290 <s21290@pjwstk.edu.pl>
2023-06-27 21:39:23 +0200

    Initial commit

---
+++ src/main/java/pl/workout/FitTrackr/FitTrackrApplication.java
@@ -0,0 +1,13 @@
+package pl.workout.FitTrackr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitTrackrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitTrackrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/workout/FitTrackr/FitTrackrApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.workout.FitTrackr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitTrackrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
