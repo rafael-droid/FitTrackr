42184fe8ecc6cb6e934920043c54c7709cdac80b
s21290 <s21290@pjwstk.edu.pl>
2023-06-28 17:07:32 +0200

    Updated files Repository

--- src/main/java/pl/workout/FitTrackr/Repository/UserRepository.java
+++ src/main/java/pl/workout/FitTrackr/Repository/UserRepository.java
@@ -3,5 +3,5 @@ package pl.workout.FitTrackr.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pl.workout.FitTrackr.Model.User;
 
-public interface UserRepository extends JpaRepository<Long, User> {
+public interface UserRepository extends JpaRepository<User, Long> {
 }
