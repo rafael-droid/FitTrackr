44dd89febb33855241bead78fff673d751773e65
s21290 <s21290@pjwstk.edu.pl>
2023-07-10 14:04:45 +0200

    Added authorized, it works but only all users

---
+++ src/main/java/pl/workout/FitTrackr/Security/SecurityConfig.java
@@ -0,0 +1,24 @@
+package pl.workout.FitTrackr.Security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http
+
+                .authorizeRequests((authz) -> authz
+                        .requestMatchers("/user/").permitAll()
+                        .requestMatchers("/api/user/**").hasRole("USER"));
+
+
+        return http.build();
+    }
+}
