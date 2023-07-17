9f5d950e3153ffbf05c49d245299c342865f2391
s21290 <s21290@pjwstk.edu.pl>
2023-07-17 15:05:22 +0200

    Security part 3

--- src/main/java/pl/workout/FitTrackr/Security/ApplicationConfig.java
+++ src/main/java/pl/workout/FitTrackr/Security/ApplicationConfig.java
@@ -1,4 +1,25 @@
 package pl.workout.FitTrackr.Security;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import pl.workout.FitTrackr.Repository.UserRepository;
+
+@Configuration
+@RequiredArgsConstructor
 public class ApplicationConfig {
+    private final UserRepository userRepository;
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        return new UserDetailsService() {
+            @Override
+            public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+                return userRepository.findByEmail(username);
+            }
+        }
+    }
 }
