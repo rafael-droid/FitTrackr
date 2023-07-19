ef1059b2541195e400142de9f9df0a1ca06932a2
s21290 <s21290@pjwstk.edu.pl>
2023-07-19 15:07:12 +0200

    Implemented RegisterRequest

--- src/main/java/pl/workout/FitTrackr/Security/Auth/RegisterRequest.java
+++ src/main/java/pl/workout/FitTrackr/Security/Auth/RegisterRequest.java
@@ -1,4 +1,17 @@
 package pl.workout.FitTrackr.Security.Auth;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class RegisterRequest {
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
 }
