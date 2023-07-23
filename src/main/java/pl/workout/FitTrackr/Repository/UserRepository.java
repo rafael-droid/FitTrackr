c2beb45b199c09472431af47eab38eff932712e8
s21290 <s21290@pjwstk.edu.pl>
2023-07-23 15:06:19 +0200

    Added optional version to findByEmail in UserRepository

--- src/main/java/pl/workout/FitTrackr/Repository/UserRepository.java
+++ src/main/java/pl/workout/FitTrackr/Repository/UserRepository.java
@@ -6,6 +6,8 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import pl.workout.FitTrackr.Model.User;
 
+import java.util.Optional;
+
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
@@ -15,5 +17,5 @@ public interface UserRepository extends JpaRepository<User, Long> {
     @Query("select u FROM user u WHere u.firstName = :firstName")
     User getUserByFirstName(@Param("firstName") String firstName);
 
-    User findByEmail(String email);
+    Optional<User> findByEmail(String email);
 }
