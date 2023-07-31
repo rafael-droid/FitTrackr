91e285f114e6a4b1fa819d72a4a7049841b0c49c
s21290 <s21290@pjwstk.edu.pl>
2023-07-31 16:24:23 +0200

    Created Service and Controller for Route

---
+++ src/main/java/pl/workout/FitTrackr/Service/RouteService.java
@@ -0,0 +1,9 @@
+package pl.workout.FitTrackr.Service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class RouteService {
+}
--- src/main/java/pl/workout/FitTrackr/Service/UserService.java
+++ src/main/java/pl/workout/FitTrackr/Service/UserService.java
@@ -3,7 +3,6 @@ package pl.workout.FitTrackr.Service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 import pl.workout.FitTrackr.Model.User;
